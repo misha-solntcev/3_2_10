0f8be1619b08b9e11eeb4d3d299b42474eddcd0d
misha-solntcev <misha.solntcev@gmail.com>
2022-08-15 10:51:35 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Вариант 10
+Дана строка.Словом текста считается любая последовательность цифр; между
+соседними словами - не менее одного пробела, за последним словом - точка. Поменять
+местами в строке первое и последнее слово.
+*/
+namespace _3_2_10
+{
+    internal class Program
+    {
+        static string myString;
+        static string FirstSecond(string myString)
+        {
+            char[] sep = { ' ', '.' };
+            string[] myWords = Program.myString.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+
+            (myWords[myWords.Length - 1], myWords[0]) = (myWords[0], myWords[myWords.Length - 1]);
+
+            Program.myString = "";
+            for (int i = 0; i < myWords.Length; i++)
+                Program.myString += myWords[i] + " ";
+
+            return Program.myString;
+        }
+
+        static void Main(string[] args)
+        {
+            Program.myString = "Первое было, а стало Последнее.";
+
+            FirstSecond(Program.myString);
+            Console.WriteLine(Program.myString);            
+
+            Console.ReadKey();
+        }
+    }
+}
